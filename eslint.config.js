f2c279e34d6529dfbaa842c1ed1b787262279da5
yugyusang <yugyusang12@gmail.com>
2025-07-13 17:29:21 +0900

    chore: 스타일린터, 리액트 아이콘 라이브러리 적용

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import importPlugin from "eslint-plugin-import";
+import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -15,14 +16,24 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      import: importPlugin,
+      "simple-import-sort": eslintPluginSimpleImportSort,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      "import-plugin": importPlugin,
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       ...importPlugin.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   }
 );
