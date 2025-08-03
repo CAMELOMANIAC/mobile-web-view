a44dcbae43c9530929c8b2668ff185ba92b1536b
yugyusang <yugyusang12@gmail.com>
2025-08-04 01:51:13 +0900

    fix: EmployeeContainer안에 스크롤을 위한 ScrollContainer 추가

--- src/routes/(isLogin)/employee/index.tsx
+++ src/routes/(isLogin)/employee/index.tsx
@@ -23,29 +23,42 @@ function RouteComponent() {
     <Page>
       <TabBar tapItems={[<p>인사</p>, <p>고용</p>]} setTab={setTap} tab={tab} />
       <EmployeeContainer>
-        <EmployeeListItem />
-        <EmployeeListItem />
-        <EmployeeListItem />
-        <EmployeeListItem />
-        <EmployeeListItem />
-        <EmployeeListItem />
-        <EmployeeListItem />
+        <ScrollContainer>
+          <EmployeeListItem />
+          <EmployeeListItem />
+          <EmployeeListItem />
+          <EmployeeListItem />
+          <EmployeeListItem />
+          <EmployeeListItem />
+          <EmployeeListItem />
+          <EmployeeListItem />
+          <EmployeeListItem />
+        </ScrollContainer>
       </EmployeeContainer>
     </Page>
   );
 }
 
 const EmployeeContainer = styled.div`
-  display: flex;
-  flex: 1 1 auto;
-  flex-direction: column;
-  align-items: flex-start;
   position: relative;
 
   width: 100%;
   height: 100%;
   /* margin-bottom: 5rem; */
+`;
+
+const ScrollContainer = styled.div`
+  display: flex;
+  flex: 1 1 auto;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  position: absolute;
+
+  width: 100%;
+  height: auto;
   padding: 1rem;
+  padding-bottom: 6rem;
 
   gap: 0.5rem;
 `;
