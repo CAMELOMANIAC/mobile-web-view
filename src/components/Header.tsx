37cb7e45aa2588bf60a3dceecb50f9d8e657c200
yugyusang <yugyusang12@gmail.com>
2025-08-04 01:33:41 +0900

    style: 헤더 스타일 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,11 +13,46 @@ export default Header;
 const HeaderContainer = styled.header`
   display: flex;
   flex-direction: row;
+  align-items: center;
   justify-content: space-between;
+  overflow: hidden;
   position: relative;
   top: 0;
   left: 0;
 
   width: 100%;
   padding: 0.5rem;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+
+  background: rgba(255, 255, 255, 0.15);
+
+  -webkit-backdrop-filter: blur(20px);
+  backdrop-filter: blur(20px);
+  transition: all 0.3s ease-in-out;
+
+  &::before {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+
+    height: 1px;
+
+    background: linear-gradient(90deg, transparent, rgba(255, 255, 255, 0.8), transparent);
+
+    content: "";
+  }
+
+  &::after {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 1px;
+    height: 100%;
+
+    background: linear-gradient(180deg, rgba(255, 255, 255, 0.8), transparent, rgba(255, 255, 255, 0.3));
+
+    content: "";
+  }
 `;
